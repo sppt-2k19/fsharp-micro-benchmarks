7550abf1f7c98f9382ce9b10ed286308d919e6bb
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-02-22 10:39:21 +0100

    adds the rest of the benchmark suite
    and results from an i5-4210U-based laptop

--- Program.fs
+++ Program.fs
@@ -16,30 +16,55 @@ let scaleVector3D scalar =
 
 let multiplyVector2D i =
     let mutable v1 = new Vector2(1.0f)
-    let mutable v2 = new Vector2(1.0f)
+    let mutable v2 = new Vector2(float32 i)
     v1 <- v1 * v2
     v1.Length()
     
 let multiplyVector3D i =
     let mutable v1 = new Vector3(1.0f)
-    let mutable v2 = new Vector3(1.0f)
+    let mutable v2 = new Vector3(float32 i)
     v1 <- v1 * v2
     v1.Length()
     
 let translateVector2D i =
     let mutable v1 = new Vector2(1.0f);
-    v1.X <- v1.X + i
+    v1.X <- v1.X + float32 i
     v1.Length()
     
 let translateVector3D i =
     let mutable v1 = new Vector3(1.0f);
-    v1.X <- v1.X + i
+    v1.X <- v1.X + float32 i
     v1.Length()
     
 let subtractVector2D i =
-    let v1 = new Vector2(1.0f)
+    let mutable v1 = new Vector2(float32 i)
     let v2 = new Vector2(1.0f)
     v1 <- v1 - v2
+    v1.Length()
+    
+let subtractVector3D i =
+    let mutable v1 = new Vector3(float32 i)
+    let v2 = new Vector3(1.0f)
+    v1 <- v1 - v2
+    v1.Length()
+    
+let lengthVector2D i =
+    let v1 = new Vector2(float32 i)
+    v1.Length()
+    
+let lengthVector3D i =
+    let v1 = new Vector3(float32 i)
+    v1.Length()
+    
+let dotProductVector2D i =
+    let v1 = new Vector2(1.0f)
+    let v2 = new Vector2(float32 i)
+    Vector2.Dot(v1, v2)
+    
+let dotProductVector3D i =
+    let v1 = new Vector2(1.0f)
+    let v2 = new Vector2(float32 i)
+    Vector2.Dot(v1, v2)
     
 let benchmark msg iterations minTime func =
     let mutable count = 1
@@ -73,11 +98,19 @@ let benchmark msg iterations minTime func =
 let main argv =
     let iterations = 5
     let maxTime = float32 (250 * 1000000)
+    let mutable result = 0.0f
     
-    let sv2d = benchmark "ScaleVector2D" iterations maxTime scaleVector2D
-    let sv3d = benchmark "ScaleVector3D" iterations maxTime scaleVector3D
-    let mv2d = benchmark "MultiplyVector2D" iterations maxTime multiplyVector2D
-    let mv3d = benchmark "MultiplyVector3D" iterations maxTime multiplyVector3D
+    result <- result + benchmark "ScaleVector2D" iterations maxTime scaleVector2D
+    result <- result + benchmark "ScaleVector3D" iterations maxTime scaleVector3D
+    result <- result + benchmark "MultiplyVector2D" iterations maxTime multiplyVector2D
+    result <- result + benchmark "MultiplyVector3D" iterations maxTime multiplyVector3D
+    result <- result + benchmark "TranslateVector2D" iterations maxTime translateVector2D
+    result <- result + benchmark "TranslateVector3D" iterations maxTime translateVector3D
+    result <- result + benchmark "SubtractVector2D" iterations maxTime subtractVector2D
+    result <- result + benchmark "SubtractVector3D" iterations maxTime subtractVector3D
+    result <- result + benchmark "LengthVector2D" iterations maxTime lengthVector2D
+    result <- result + benchmark "LengthVector3D" iterations maxTime lengthVector3D
+    result <- result + benchmark "DotProductVector2D" iterations maxTime dotProductVector2D
+    result <- result + benchmark "DotProductVector2D" iterations maxTime dotProductVector3D
     
-    printfn "%O %O %O %O" sv2d sv3d mv2d mv3d
     0 // return an integer exit code
