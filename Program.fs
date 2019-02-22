2287b3fb9e3a6253e369aa374cb32caf446690f8
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-02-22 11:06:10 +0100

    more results
    and non-mutating benchmarks

--- Program.fs
+++ Program.fs
@@ -1,70 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module main
 
 open System
-open System
-open System.Numerics
+open NoMutateBenchmarks
 
-let scaleVector2D scalar =
-    let mutable v1 = new Vector2(1.0f)
-    v1 <- v1 * float32 scalar
-    v1.Length()
-
-let scaleVector3D scalar =
-    let mutable v1 = new Vector3(1.0f)
-    v1 <- v1 * float32 scalar
-    v1.Length()
-
-let multiplyVector2D i =
-    let mutable v1 = new Vector2(1.0f)
-    let mutable v2 = new Vector2(float32 i)
-    v1 <- v1 * v2
-    v1.Length()
-    
-let multiplyVector3D i =
-    let mutable v1 = new Vector3(1.0f)
-    let mutable v2 = new Vector3(float32 i)
-    v1 <- v1 * v2
-    v1.Length()
-    
-let translateVector2D i =
-    let mutable v1 = new Vector2(1.0f);
-    v1.X <- v1.X + float32 i
-    v1.Length()
-    
-let translateVector3D i =
-    let mutable v1 = new Vector3(1.0f);
-    v1.X <- v1.X + float32 i
-    v1.Length()
-    
-let subtractVector2D i =
-    let mutable v1 = new Vector2(float32 i)
-    let v2 = new Vector2(1.0f)
-    v1 <- v1 - v2
-    v1.Length()
-    
-let subtractVector3D i =
-    let mutable v1 = new Vector3(float32 i)
-    let v2 = new Vector3(1.0f)
-    v1 <- v1 - v2
-    v1.Length()
-    
-let lengthVector2D i =
-    let v1 = new Vector2(float32 i)
-    v1.Length()
-    
-let lengthVector3D i =
-    let v1 = new Vector3(float32 i)
-    v1.Length()
-    
-let dotProductVector2D i =
-    let v1 = new Vector2(1.0f)
-    let v2 = new Vector2(float32 i)
-    Vector2.Dot(v1, v2)
-    
-let dotProductVector3D i =
-    let v1 = new Vector2(1.0f)
-    let v2 = new Vector2(float32 i)
-    Vector2.Dot(v1, v2)
     
 let benchmark msg iterations minTime func =
     let mutable count = 1
