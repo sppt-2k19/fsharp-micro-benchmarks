9ed53d64f7aaaf50d731c7afa7f92bae26b97ceb
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-03-04 00:42:21 +0100

    both implementation gives similar results

--- src/MutateBenchmarks.fs
+++ src/MutateBenchmarks.fs
@@ -1,66 +1,90 @@
 module MutateBenchmarks
-open System.Numerics
-
-let scaleVector2D scalar =
-    let mutable v1 = new Vector2(1.0f)
-    v1 <- v1 * float32 scalar
-    v1.Length()
-
-let scaleVector3D scalar =
-    let mutable v1 = new Vector3(1.0f)
-    v1 <- v1 * float32 scalar
-    v1.Length()
-
-let multiplyVector2D i =
-    let mutable v1 = new Vector2(1.0f)
-    let mutable v2 = new Vector2(float32 i)
-    v1 <- v1 * v2
-    v1.Length()
+    open System.Numerics
     
-let multiplyVector3D i =
-    let mutable v1 = new Vector3(1.0f)
-    let mutable v2 = new Vector3(float32 i)
-    v1 <- v1 * v2
-    v1.Length()
+    let scaleVector2D scalar =
+        let mutable v1 = new Vector2(1.0f)
+        v1 <- v1 * float32 scalar
+        v1.Length()
     
-let translateVector2D i =
-    let mutable v1 = new Vector2(1.0f);
-    let mutable v2 = new Vector2(float32 i);
-    let v3 = v1 + v2
-    v3.Length()
+    let scaleVector3D scalar =
+        let mutable v1 = new Vector3(1.0f)
+        v1 <- v1 * float32 scalar
+        v1.Length()
     
-let translateVector3D i =
-    let mutable v1 = new Vector3(1.0f);
-    let mutable v2 = new Vector3(float32 i);
-    let v3 = v1 + v2
-    v3.Length()
+    let multiplyVector2D i =
+        let mutable v1 = new Vector2(1.0f)
+        let mutable v2 = new Vector2(float32 i)
+        v1 <- v1 * v2
+        v1.Length()
+        
+    let multiplyVector3D i =
+        let mutable v1 = new Vector3(1.0f)
+        let mutable v2 = new Vector3(float32 i)
+        v1 <- v1 * v2
+        v1.Length()
+        
+    let translateVector2D i =
+        let mutable v1 = new Vector2(1.0f);
+        let mutable v2 = new Vector2(float32 i);
+        let v3 = v1 + v2
+        v3.Length()
+        
+    let translateVector3D i =
+        let mutable v1 = new Vector3(1.0f);
+        let mutable v2 = new Vector3(float32 i);
+        let v3 = v1 + v2
+        v3.Length()
+        
+    let subtractVector2D i =
+        let mutable v1 = new Vector2(float32 i)
+        let v2 = new Vector2(1.0f)
+        v1 <- v1 - v2
+        v1.Length()
+        
+    let subtractVector3D i =
+        let mutable v1 = new Vector3(float32 i)
+        let v2 = new Vector3(1.0f)
+        v1 <- v1 - v2
+        v1.Length()
+        
+    let lengthVector2D i =
+        let v1 = new Vector2(float32 i)
+        v1.Length()
+        
+    let lengthVector3D i =
+        let v1 = new Vector3(float32 i)
+        v1.Length()
+        
+    let dotProductVector2D i =
+        let v1 = new Vector2(1.0f)
+        let v2 = new Vector2(float32 i)
+        Vector2.Dot(v1, v2)
+        
+    let dotProductVector3D i =
+        let v1 = new Vector3(1.0f)
+        let v2 = new Vector3(float32 i)
+        Vector3.Dot(v1, v2)
+        
+    let rec sieve = function
+        | (p::xs) -> p :: sieve [ for x in xs do if x % p > 0 then yield x ]
+        | []      -> []
+        
+    let multiply i =
+        let x = 1.1f * float32 (i &&& 0xFF)
+        x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x
+        
+    let fibonacci =
+        Seq.unfold
+            (fun (current, next) -> Some(current, (next, current + next)))
+            (0, 1)
     
-let subtractVector2D i =
-    let mutable v1 = new Vector2(float32 i)
-    let v2 = new Vector2(1.0f)
-    v1 <- v1 - v2
-    v1.Length()
-    
-let subtractVector3D i =
-    let mutable v1 = new Vector3(float32 i)
-    let v2 = new Vector3(1.0f)
-    v1 <- v1 - v2
-    v1.Length()
-    
-let lengthVector2D i =
-    let v1 = new Vector2(float32 i)
-    v1.Length()
-    
-let lengthVector3D i =
-    let v1 = new Vector3(float32 i)
-    v1.Length()
-    
-let dotProductVector2D i =
-    let v1 = new Vector2(1.0f)
-    let v2 = new Vector2(float32 i)
-    Vector2.Dot(v1, v2)
-    
-let dotProductVector3D i =
-    let v1 = new Vector3(1.0f)
-    let v2 = new Vector3(float32 i)
-    Vector3.Dot(v1, v2)
+    let fibonacci2 digits =
+        let mutable a = 0
+        let mutable b = 1
+        let mutable c = 0
+        
+        for j in 0 .. digits do
+            c <- a + b
+            a <- b
+            b <- c
+        c
--- src/Program.fs
+++ src/Program.fs
@@ -1,55 +1,95 @@
 ﻿module Main
-
-open System
-open MutateBenchmarks
-
+    open System
+    open MutateBenchmarks
     
-let benchmark msg iterations minTime func =
-    let mutable count = 1
-    let mutable totalCount = 0
-    let mutable dummy = 0.0f
-    let mutable runningTime = 0.0f
-    let mutable deltaTime = 0.0f
-    let mutable deltaTimeSquared = 0.0f
+
+    let iterativeBenchmark msg func iterations minTime =
+        let mutable count = 1
+        let mutable runningTime = 0.0
+        let mutable deltaTime = 0.0
+        let mutable deltaTimeSquared = 0.0
+        
+        while (runningTime < minTime && count < Int32.MaxValue / 2) do
+            count <- count * 2
+            deltaTime <- 0.0
+            deltaTimeSquared <- 0.0 
+            for j in 0 .. iterations do
+                let started = DateTime.UtcNow
+                for i in 0 .. count do
+                    func i |> ignore
+                runningTime <- DateTime.UtcNow.Subtract(started).TotalMilliseconds * 1000000.0
+                let time = runningTime / float count
+                deltaTime <- deltaTime + time
+                deltaTimeSquared <- deltaTimeSquared + time * time
+        let mean = deltaTime / float iterations
+        let standardDeviation = sqrt (abs (deltaTimeSquared - mean * mean * float iterations) / float (iterations - 1))
+        printfn "%s,%f,%f,%i" msg mean standardDeviation count
+        msg, mean, standardDeviation, count
     
-    while (runningTime < minTime && count < Int32.MaxValue / 2) do
-        count <- count * 2
-        deltaTime <- 0.0f
-        deltaTimeSquared <- 0.0f
-        for j in 0 .. iterations do
-            let timer = System.Diagnostics.Stopwatch.StartNew()
+    let runIterations func iterations count =
+        let reducer acc elem =
+            let started = DateTime.UtcNow
             for i in 0 .. count do
-                dummy <- dummy + func i
-            runningTime <- float32 timer.ElapsedTicks * 100.0f
-            let time = runningTime / (float32 count)
-            deltaTime <- deltaTime + time
-            deltaTimeSquared <- deltaTime * deltaTime
-            totalCount <- totalCount + count
-    let mean = deltaTime / float32 iterations
-    let standardDeviation = sqrt (deltaTimeSquared - mean * mean * float32 iterations) / float32 (iterations - 1)
-    printfn "%s,%f,%f,%i" msg mean standardDeviation count
-    dummy / float32 totalCount
+                func i |> ignore
+            let spentTime = DateTime.UtcNow.Subtract(started).TotalMilliseconds * 1000000.0
+            let time = spentTime / float count
+            let deltaTime, deltaTimeSquared, runningTime = acc
+            deltaTime + time, deltaTimeSquared + (time * time), spentTime
+        List.fold reducer (0.0, 0.0, 0.0) [0 .. iterations]
     
+    let rec recursiveBenchmark (msg, func, iterations, minTime, count)  =
+        let deltaTime, deltaTimeSquared, runningTime = runIterations func iterations count
+        
+        if runningTime < minTime && count < Int32.MaxValue / 2 then
+            recursiveBenchmark (msg, func, iterations, minTime, count * 2)
+        else 
+            let mean = deltaTime / float iterations
+            let standardDeviation = sqrt (abs (deltaTimeSquared - mean * mean * float iterations) / float (iterations - 1))
+            printfn "%s,%f,%f,%i" msg mean standardDeviation count
+            msg, mean, standardDeviation, count
+                
     
-    
-[<EntryPoint>]
-let main argv =
-    let iterations = 5
-    let maxTime = float32 (250 * 1000000)
-    let mutable result = 0.0f
-    
-    result <- result + benchmark "ScaleVector2D" iterations maxTime scaleVector2D
-    result <- result + benchmark "ScaleVector3D" iterations maxTime scaleVector3D
-    result <- result + benchmark "MultiplyVector2D" iterations maxTime multiplyVector2D
-    result <- result + benchmark "MultiplyVector3D" iterations maxTime multiplyVector3D
-    result <- result + benchmark "TranslateVector2D" iterations maxTime translateVector2D
-    result <- result + benchmark "TranslateVector3D" iterations maxTime translateVector3D
-    result <- result + benchmark "SubtractVector2D" iterations maxTime subtractVector2D
-    result <- result + benchmark "SubtractVector3D" iterations maxTime subtractVector3D
-    result <- result + benchmark "LengthVector2D" iterations maxTime lengthVector2D
-    result <- result + benchmark "LengthVector3D" iterations maxTime lengthVector3D
-    result <- result + benchmark "DotProductVector2D" iterations maxTime dotProductVector2D
-    result <- result + benchmark "DotProductVector3D" iterations maxTime dotProductVector3D
-    
-    printfn "\n%f" result
-    0 // return an integer exit code
+    [<EntryPoint>]
+    let main argv =
+        let iterations = 5
+        let minTime = float (250 * 1000000)
+        let mutable results = []
+        
+        let startRecursiveBench msg func iterations minTime = recursiveBenchmark (msg, func, iterations, minTime, 1)
+        let runBenchmark bench msg func = bench msg func iterations minTime
+        
+        let (<<) l r = results <- results @ [ r ]
+        
+        
+        printfn "Iterative Mark8 benchmark"
+        results << runBenchmark iterativeBenchmark "ScaleVector2D" scaleVector2D  
+        results << runBenchmark iterativeBenchmark "ScaleVector3D" scaleVector3D 
+        results << runBenchmark iterativeBenchmark "MultiplyVector2D" multiplyVector2D
+        results << runBenchmark iterativeBenchmark "MultiplyVector3D" multiplyVector3D
+        results << runBenchmark iterativeBenchmark "TranslateVector2D" translateVector2D  
+        results << runBenchmark iterativeBenchmark "TranslateVector3D" translateVector3D
+        results << runBenchmark iterativeBenchmark "SubtractVector2D" subtractVector2D
+        results << runBenchmark iterativeBenchmark "SubtractVector3D" subtractVector3D
+        results << runBenchmark iterativeBenchmark "LengthVector2D" lengthVector2D
+        results << runBenchmark iterativeBenchmark "LengthVector3D" lengthVector3D
+        results << runBenchmark iterativeBenchmark "DotProductVector2D" dotProductVector2D  
+        results << runBenchmark iterativeBenchmark "DotProductVector3D" dotProductVector3D
+        
+        printfn "\nRecursive Mark8 benchmark"
+        results << runBenchmark startRecursiveBench "ScaleVector2D" scaleVector2D
+        results << runBenchmark startRecursiveBench "ScaleVector3D" scaleVector3D
+        results << runBenchmark startRecursiveBench "MultiplyVector2D" multiplyVector2D
+        results << runBenchmark startRecursiveBench "MultiplyVector3D" multiplyVector3D
+        results << runBenchmark startRecursiveBench "TranslateVector2D" translateVector2D
+        results << runBenchmark startRecursiveBench "TranslateVector3D" translateVector3D
+        results << runBenchmark startRecursiveBench "SubtractVector2D" subtractVector2D
+        results << runBenchmark startRecursiveBench "SubtractVector3D" subtractVector3D
+        results << runBenchmark startRecursiveBench "LengthVector2D" lengthVector2D
+        results << runBenchmark startRecursiveBench "LengthVector3D" lengthVector3D
+        results << runBenchmark startRecursiveBench "DotProductVector2D" dotProductVector2D
+        results << runBenchmark startRecursiveBench "DotProductVector3D" dotProductVector3D
+        
+        
+        
+        printfn "\n%O" results
+        0 // return an integer exit code
