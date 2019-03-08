1ee551cca238ed319a68edef0183126c737ab710
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-03-08 16:50:47 +0100

    Adds fibonacci benchmarks

--- src/NumericalBenchmarks.fs
+++ src/NumericalBenchmarks.fs
@@ -5,20 +5,35 @@ module NumericalBenchmarks
         let x = 1.1f * float32 (i &&& 0xFF)
         x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x
         
-    let fibonacciRec (i:int):float32 =
-        (Seq.unfold
-            (fun (current, next) -> Some(current, (next, current + next)))
-            (0, 1))
-        |> Seq.take 50 |> Seq.head |> float32
     
-    let fibonacciIterative (i:int):float32 =
-        let mutable a = 0
-        let mutable b = 1
-        let mutable c = 0
+    
         
-        (seq { for j in 0 .. Int32.MaxValue do
+    let fibRecWrap n =
+        let rec fibonacciRec current next n =
+            match n with
+            | 0 -> current + next
+            | _ -> fibonacciRec next (current + next) (n - 1)
+        float32 (fibonacciRec 0 1 200)
+        
+    let fibIterWrap n =
+        let fibonacciIterative n =
+            let mutable a = 0
+            let mutable b = 1
+            let mutable c = 0
+            
+            for j in 2 .. n do
                 c <- a + b
                 a <- b
                 b <- c
-                yield c })
-        |> Seq.take 50 |> Seq.head |> float32
+            c
+        float32 (fibonacciIterative 200)
+        
+    let sieve_primes top_number = 
+        let numbers = [ yield 2
+                        for i in 3..2..top_number -> i ]
+        let rec sieve ns = 
+            match ns with
+            | [] -> []
+            | x::xs when x*x > top_number -> ns
+            | x::xs -> x::sieve (List.filter(fun y -> y%x <> 0) xs)
+        sieve numbers 
--- src/Program.fs
+++ src/Program.fs
@@ -4,12 +4,11 @@
     
 
     open System
-    let iterativeBenchmark(msg:string, func:int -> float32, iterations:int32, minTime:float): string * float * float * int * float =
+    let iterativeBenchmark msg func iterations minTime =
         let mutable count = 1
         let mutable runningTime = 0.0
         let mutable deltaTime = 0.0
         let mutable deltaTimeSquared = 0.0
-        let mutable result = 0.0f
         
         while (runningTime < minTime && count < Int32.MaxValue / 2) do
             count <- count * 2
@@ -18,38 +17,36 @@
             for j in 0 .. iterations do
                 let started = DateTime.UtcNow
                 for i in 0 .. count do
-                    result <- result + func i
+                    func i |> ignore
                 runningTime <- DateTime.UtcNow.Subtract(started).TotalMilliseconds * 1000000.0
                 let time = runningTime / float count
                 deltaTime <- deltaTime + time
                 deltaTimeSquared <- deltaTimeSquared + time * time
         let mean = deltaTime / float iterations
         let standardDeviation = sqrt (abs (deltaTimeSquared - mean * mean * float iterations) / float (iterations - 1))
-        printfn "%s,%f,%f,%i" msg mean standardDeviation count
-        msg, mean, standardDeviation, count, result
+        printfn "%s,%.3f,%.3f,%i" msg mean standardDeviation count
+        msg, mean, standardDeviation, count
     
-    let runIterations func iterations count =
-        let reducer acc elem =
-            let started = DateTime.UtcNow
-            let mutable result = 0.0
-            for i in 0 .. count do
-                result <- result + func i
-            let runningTime = DateTime.UtcNow.Subtract(started).TotalMilliseconds * 1000000.0
-            let time = runningTime / float count
-            let deltaTime, deltaTimeSquared, _, _ = acc
-            deltaTime + time, deltaTimeSquared + (time * time), runningTime, result
-        List.fold reducer (0.0, 0.0, 0.0, 0.0) [0 .. iterations]
-    
-    let rec recursiveBenchmark (msg:string, func:int32 -> float, iterations:int32, minTime:float, count:int32): string * float * float * int * float =
-        let deltaTime, deltaTimeSquared, runningTime, result = runIterations func iterations count
-        
-        if runningTime < minTime && count < Int32.MaxValue / 2 then
-            recursiveBenchmark (msg, func, iterations, minTime, count * 2)
-        else 
-            let mean = deltaTime / float iterations
-            let standardDeviation = sqrt (abs (deltaTimeSquared - mean * mean * float iterations) / float (iterations - 1))
-            printfn "%s,%f,%f,%i" msg mean standardDeviation count
-            msg, mean, standardDeviation, count, result
+//    let runIterations func iterations count =
+//        let reducer acc elem =
+//            let started = DateTime.UtcNow
+//            for i in 0 .. count do
+//                    func i |> ignore
+//            let runningTime = DateTime.UtcNow.Subtract(started).TotalMilliseconds * 1000000.0
+//            let time = runningTime / float count
+//            let deltaTime, deltaTimeSquared, _ = acc
+//            deltaTime + time, deltaTimeSquared + (time * time), runningTime
+//        List.fold reducer (0.0, 0.0, 0.0) [0 .. iterations]
+//    let rec recursiveBenchmark msg func iterations minTime count =
+//        let deltaTime, deltaTimeSquared, runningTime = runIterations func iterations count
+//        
+//        if runningTime < minTime && count < Int32.MaxValue / 2 then
+//            recursiveBenchmark msg func iterations minTime count * 2
+//        else 
+//            let mean = deltaTime / float iterations
+//            let standardDeviation = sqrt (abs (deltaTimeSquared - mean * mean * float iterations) / float (iterations - 1))
+//            printfn "%s,%f,%f,%i" msg mean standardDeviation count
+//            msg, mean, standardDeviation, count
                 
     
     [<EntryPoint>]
@@ -58,43 +55,46 @@
         let minTime = float (250 * 1000000)
         let mutable results = []
         
-        let startRecursiveBench (msg:string, func:int -> float32, iterations:int32, minTime: float) = recursiveBenchmark (msg, func, iterations, minTime, 1)
-        let runBenchmark (bench:string * (int -> float32) * int32 * float -> string * float * float * int * float32, msg:string, func:int->float32) =
-            bench(msg, func, iterations, minTime)
+//        let startRecursiveBench msg func iterations minTime = recursiveBenchmark msg func iterations minTime 1
+        let runBenchmark bench msg func = bench msg func iterations minTime
         
         let (<<) l r = results <- results @ [ r ]
         
         
         printfn "Iterative Mark8 benchmark"
-        results << runBenchmark (iterativeBenchmark, "FibonacciRec", fibonacciRec)
-        results << runBenchmark (iterativeBenchmark, "FibonacciIter", fibonacciIterative)
+        results << runBenchmark iterativeBenchmark "FibonacciRec" fibRecWrap
+        results << runBenchmark iterativeBenchmark "FibonacciRec" fibRecWrap
+        results << runBenchmark iterativeBenchmark "FibonacciRec" fibRecWrap
+        results << runBenchmark iterativeBenchmark "FibonacciIter" fibIterWrap
+        results << runBenchmark iterativeBenchmark "FibonacciIter" fibIterWrap
+        results << runBenchmark iterativeBenchmark "FibonacciIter" fibIterWrap
         
-        results << runBenchmark (iterativeBenchmark, "ScaleVector2D", scaleVector2D)
-        results << runBenchmark (iterativeBenchmark, "ScaleVector3D", scaleVector3D)
-        results << runBenchmark (iterativeBenchmark, "MultiplyVector2D", multiplyVector2D)
-        results << runBenchmark (iterativeBenchmark, "MultiplyVector3D", multiplyVector3D)
-        results << runBenchmark (iterativeBenchmark, "TranslateVector2D", translateVector2D)
-        results << runBenchmark (iterativeBenchmark, "TranslateVector3D", translateVector3D)
-        results << runBenchmark (iterativeBenchmark, "SubtractVector2D", subtractVector2D)
-        results << runBenchmark (iterativeBenchmark, "SubtractVector3D", subtractVector3D)
-        results << runBenchmark (iterativeBenchmark, "LengthVector2D", lengthVector2D)
-        results << runBenchmark (iterativeBenchmark, "LengthVector3D", lengthVector3D)
-        results << runBenchmark (iterativeBenchmark, "DotProductVector2D", dotProductVector2D)
-        results << runBenchmark (iterativeBenchmark, "DotProductVector3D", dotProductVector3D)
+        results << runBenchmark iterativeBenchmark "ScaleVector2D" scaleVector2D
+        results << runBenchmark iterativeBenchmark "ScaleVector3D" scaleVector3D 
+        results << runBenchmark iterativeBenchmark "MultiplyVector2D" multiplyVector2D
+        results << runBenchmark iterativeBenchmark "MultiplyVector3D" multiplyVector3D
+        results << runBenchmark iterativeBenchmark "TranslateVector2D" translateVector2D  
+        results << runBenchmark iterativeBenchmark "TranslateVector3D" translateVector3D
+        results << runBenchmark iterativeBenchmark "SubtractVector2D" subtractVector2D
+        results << runBenchmark iterativeBenchmark "SubtractVector3D" subtractVector3D
+        results << runBenchmark iterativeBenchmark "LengthVector2D" lengthVector2D
+        results << runBenchmark iterativeBenchmark "LengthVector3D" lengthVector3D
+        results << runBenchmark iterativeBenchmark "DotProductVector2D" dotProductVector2D  
+        results << runBenchmark iterativeBenchmark "DotProductVector3D" dotProductVector3D
         
-        printfn "\nRecursive Mark8 benchmark"
-        results << runBenchmark (startRecursiveBench, "ScaleVector2D", scaleVector2D)
-        results << runBenchmark (startRecursiveBench, "ScaleVector3D", scaleVector3D)
-        results << runBenchmark (startRecursiveBench, "MultiplyVector2D", multiplyVector2D)
-        results << runBenchmark (startRecursiveBench, "MultiplyVector3D", multiplyVector3D)
-        results << runBenchmark (startRecursiveBench, "TranslateVector2D", translateVector2D)
-        results << runBenchmark (startRecursiveBench, "TranslateVector3D", translateVector3D)
-        results << runBenchmark (startRecursiveBench, "SubtractVector2D", subtractVector2D)
-        results << runBenchmark (startRecursiveBench, "SubtractVector3D", subtractVector3D)
-        results << runBenchmark (startRecursiveBench, "LengthVector2D", lengthVector2D)
-        results << runBenchmark (startRecursiveBench, "LengthVector3D", lengthVector3D)
-        results << runBenchmark (startRecursiveBench, "DotProductVector2D", dotProductVector2D)
-        results << runBenchmark (startRecursiveBench, "DotProductVector3D", dotProductVector3D)
+//        printfn "\nRecursive Mark8 benchmark"
+//        results << runBenchmark startRecursiveBench "ScaleVector2D" scaleVector2D
+//        results << runBenchmark startRecursiveBench "ScaleVector3D" scaleVector3D
+//        results << runBenchmark startRecursiveBench "MultiplyVector2D" multiplyVector2D
+//        results << runBenchmark startRecursiveBench "MultiplyVector3D" multiplyVector3D
+//        results << runBenchmark startRecursiveBench "TranslateVector2D" translateVector2D
+//        results << runBenchmark startRecursiveBench "TranslateVector3D" translateVector3D
+//        results << runBenchmark startRecursiveBench "SubtractVector2D" subtractVector2D
+//        results << runBenchmark startRecursiveBench "SubtractVector3D" subtractVector3D
+//        results << runBenchmark startRecursiveBench "LengthVector2D" lengthVector2D
+//        results << runBenchmark startRecursiveBench "LengthVector3D" lengthVector3D
+//        results << runBenchmark startRecursiveBench "DotProductVector2D" dotProductVector2D
+//        results << runBenchmark startRecursiveBench "DotProductVector3D" dotProductVector3D
         
         
         
