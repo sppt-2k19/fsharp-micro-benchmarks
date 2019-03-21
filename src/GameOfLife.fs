c38ede39ef2546f4042f18e15071ff4cf3c78733
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-03-21 15:08:13 +0100

    Game of life added

--- src/GameOfLife.fs
+++ src/GameOfLife.fs
@@ -8,9 +8,8 @@ let defaultGameOfLifeGrid = "
 00101
 "
 
-let iterateGameOfLife (grid:string) =
+let iterateGrid (grid:string) =
     let lines = grid.Split([|'\r';'\n'|], StringSplitOptions.RemoveEmptyEntries)
-    let lines = lines |> Array.filter (fun line -> line.Length > 0)
     let width = lines.[0].Length
     let height = lines.Length
     let computeNeighbours x y =
@@ -18,23 +17,28 @@ let iterateGameOfLife (grid:string) =
          -1, 0;       1, 0
          -1, 1; 0, 1; 1, 1]
         |> List.map (fun (dx,dy) ->
-            let x, y = x + dx, y + dy
-            if x >= 0 && x < width && y >= 0 && y < height && lines.[y].Chars(x) = '1'
-                then 1
-                else 0
+            let nx,ny = x+dx, y+dy
+            if nx >=0 && nx < width && ny >= 0 && ny < height &&
+               lines.[ny].Chars(nx) = '1'
+            then 1
+            else 0
         ) |> List.sum
 
-    let life y x c =       
+    let life x y c =       
         match c, computeNeighbours x y with
-        | '1', 2 ->  c
+        | '1' ,2 ->  c
         |  _ , 3 -> '1'
         |  _ , _ -> '0'
     
     
-    lines |> Array.mapi (fun y line -> line.ToCharArray() |> Array.mapi (life y) |> String) |> String.concat "\r\n"
-    
+    let newLines = lines |> Array.mapi (fun y line ->
+        let chars = line.ToCharArray()
+        let values = Array.mapi (fun x c -> life x y c) chars
+        String(values)
+    )
+    String.Join("\r\n", newLines)
     
 let iterateGameOfLifeTimes times x =
     let mutable grid = defaultGameOfLifeGrid
-    for i in 0..times do grid <- iterateGameOfLife grid
+    for i in 0..times do grid <- iterateGrid grid
     0.0f
